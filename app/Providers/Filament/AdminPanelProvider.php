0f45c78d2c40817635bc37c994a17b9cc2c21967
Ivaaaan02 <ivanaldousvillafuerte2@gmail.com>
2024-12-13 10:10:23 +0800

    Integrated Filamentphp plugin shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -80,8 +81,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
 
+        ->plugins([
+                FilamentShieldPlugin::make(),
+        ]);
             
     }
 }
